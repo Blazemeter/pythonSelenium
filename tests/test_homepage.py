18d391b4b4ac4fc574d8107a5fd2f7c5484ec966
Eliran Shani <eliran.shani@gmail.com>
2021-12-11 23:00:10 +0200

    Update tests

--- tests/test_homepage.py
+++ tests/test_homepage.py
@@ -4,13 +4,13 @@ from tests.test_base import BaseTest
 
 class TestHomePage(BaseTest):
 
-    def test_verify_version_to_pass(self):
+    def test_verify_version_passed(self):
         self.homepage = HomePage(self.driver)
 
         assert '2.1.0.7' in self.homepage.get_version()
 
     # For demo blazemeter report purpose
-    def test_verify_version_to_fail(self):
+    def test_verify_version_failed(self):
         self.homepage = HomePage(self.driver)
 
-        assert 'some wrong version' in self.homepage.get_version()
+        assert 'some wrong value' in self.homepage.get_version()
